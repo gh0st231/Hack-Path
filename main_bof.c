4e3d4895c3015bb77cbd6bb41fb49b8a4d660fe1
ghozt <36374295+ghozt123@users.noreply.github.com>
2018-02-12 16:03:08 +0100

    Add files via upload

---
+++ main_bof.c
@@ -0,0 +1,28 @@
+// DEVELOPED by ghozt123
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+  int var;
+  int check = 0x041414141;
+  char buf[40];
+
+  fgets(buf,45,stdin);
+
+  printf("\n[buf]: %s\n", buf);
+  printf("[check] %p\n", check);
+
+  if ((check != 0x041414141) && (check != 0xcafecafe))
+    printf ("\nVous etes sur la bonne voie !\n");
+
+  if (check == 0xcafecafe)
+   {
+     printf("Vous avez gagné !\nOuverture du shell ...\n");
+     system("/bin/dash");
+     printf("Fermeture du shell! Bye.\n");
+   }
+   return 0;
+}
